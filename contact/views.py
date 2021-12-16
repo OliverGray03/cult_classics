5a04395cddc440b22a64fbe864d80e78517cfcdc
OliverGray03 <Oliver.gray90@outlook.com>
2021-12-16 20:41:35 +0000

    Added contact form view, need to add message templates

--- contact/views.py
+++ contact/views.py
@@ -9,5 +9,43 @@ def contact(request):
     """
     A view to display the contact us page and contact form
     """
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = request.POST.get('subject')
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            form.save()
+            messages.success(
+                request,
+                f'Thanks {name}! {subject} has been sent to the \
+                    Tarmachan team.'
+            )
+            # Email confirmation of contact message
+            data = form.save()
+            subject = render_to_string(
+                'contact/confirmation_emails/contact_message_subject.txt',
+                {'data': data}
+            )
+            body = render_to_string(
+                'contact/confirmation_emails/contact_message_body.txt',
+                {'data': data}
+            )
+            send_mail(
+                subject,
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [email],
+            )
+        else:
+            messages.error(
+                request,
+                "Failed to send message, please ensure all fields are correct"
+            )
 
-    return render(request, 'contact/contact.html')
+    form = ContactForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'contact/contact.html', context)
