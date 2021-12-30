5a50723357c3d104d12383d87b8cf4cf93e5172d
OliverGray03 <Oliver.gray90@outlook.com>
2021-12-30 21:21:13 +0000

    PEP8 Compliance formatting

--- favorites/models.py
+++ favorites/models.py
@@ -12,7 +12,4 @@ class Favorite(models.Model):
         UserProfile, null=False, blank=False, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "user: " (
-            + self.user_profile.user.username + ",
-            product: " + self.product.name
-        )
+        return "user: " + self.user_profile.user.username + ", product: " + self.product.name
